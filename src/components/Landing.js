b1c002d31e65a5a9e660b3c7b41aee4d9d5ad05e
bly01854 <O80696o80696>
2018-04-26 19:04:04 -0500

    added github pages

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -41,7 +41,6 @@ class Landing extends Component {
               </div>
             </div>
             </Col>
-<<<<<<< HEAD
           </Col>
           <Col md={6} lg={6} className="text-center transparent">
             <Row>
@@ -124,26 +123,6 @@ class Landing extends Component {
             </Row>
           </Col>
           <Col xsHidden sm={2} md={2} lg={3} className="">
-=======
-          </Col>
-          <Col md={6} lg={6} className="text-center transparent">
-            <Row>
-              <h1 className="font-mission">MISSION</h1>
-              <h1 className="font-to-the">TO THE</h1>
-              <img className="logo" src={require("../assets/logo.png")} alt="Mission to the Moon" />
-              <h1 className="font-get-moving">LET'S GET MOVING!</h1>
-              <Row >
-              <FontAwesome name="facebook" inverse="true" size="3x" className="social-media-row" />
-              <FontAwesome name="twitter" inverse="true" size="3x" className="social-media-row" />
-              <FontAwesome name="instagram" inverse="true" size="3x" className="social-media-row" />
-              </Row>
-              
-            </Row>
-          </Col>
-          <Col md={3} lg={3}>
-            {this.props.loggedIn == true && 
-            <UserInput/>}
->>>>>>> master
           </Col>
         </Row>
       </Grid>
